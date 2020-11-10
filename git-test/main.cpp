e77c2b88f871b194b6f39d53a2b9e1b2a978379c
GaryWong-hub <819523900@qq.com>
2020-11-10 14:56:11 +0800

    Initial Commit

---
+++ git-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git-test
+//
+//  Created by 王国立 on 2020/11/10.
+//  Copyright © 2020 王国立. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
